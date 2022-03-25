0247eb01a5cd31b87b97f04ba5c5728e5e48ad0a
AlexCabrera123 <l21121600@morelia.tecnm.mx>
2022-03-25 11:48:25 -0600

    Commit 4

--- src/Calculadora.java
+++ src/Calculadora.java
@@ -40,4 +40,45 @@ public class Calculadora {
         this.marca = marca;
         this.esCientifica = esCientifica;
     }
+
+    public double sumar() {
+
+        return primerValor + segundoValor;
+    }
+
+    public double restar() {
+        return primerValor - segundoValor;
+    }
+
+    public double multiplicar() {
+        return primerValor * segundoValor;
+    }
+
+    public double dividir() {
+        double division;
+        division = primerValor / segundoValor;
+
+        if (segundoValor == 0) {
+            return 0.0;
+        } else {
+            return division;
+        }
+    }
+
+        public String elevarAPotencia(){
+        if(esCientifica==true){
+            return " "+Math.pow(primerValor,segundoValor);
+        }else{
+            return "Su calculadora no es científica";
+        }
+        }
+
+        public String toString(){
+            if(esCientifica==true){
+                return "La calculadora es cientifica y es de marca " +marca;
+            }else{
+                return "La calculadora no es cientifica y es de marca " +marca;
+            }
+
+    }
 }
